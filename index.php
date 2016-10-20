bf7bb08f43246768740a235666267619cb033d99
kazes <florian.boudot@gmail.com>
2016-10-20 16:09:11 +0200

    remove unused scss files and vars

--- index.php
+++ index.php
@@ -10,106 +10,102 @@
 <div class="main-container">
 
     <!-- STORE LOCATOR -->
-    <div class="section section-small-m-full">
-        <div class="section-inner">
-            <div class="section-content">
-                <div class="main-head ">
 
-                    <!-- FILTER MARKERS -->
-                    <div class="filter-markers">
-                        <p class="btn-number-product btn btn-medium js-filter-markers bt-filter filter-all active" data-place-type="all">
-                            all markers
-                        </p>
-                        <p class="btn-number-product btn btn-medium js-filter-markers bt-filter filter-agences" data-place-type="1">
-                            Blue markers
-                        </p>
-                        <p class="btn-number-product btn btn-medium js-filter-markers bt-filter filter-showrooms" data-place-type="0">
-                            Red Markers
-                        </p>
-                    </div>
-                </div>
 
-                <!-- coords from url -->
-                <?php $lat = isset($_GET['lat']) ? $_GET['lat'] : ''; ?>
-                <?php $lng = isset($_GET['lng']) ? $_GET['lng'] : ''; ?>
+    <!-- FILTER MARKERS -->
+    <div class="filter-markers">
+        <p class="btn-number-product btn btn-medium js-filter-markers bt-filter filter-all active" data-place-type="all">
+            all markers
+        </p>
+        <p class="btn-number-product btn btn-medium js-filter-markers bt-filter filter-agences" data-place-type="1">
+            Blue markers
+        </p>
+        <p class="btn-number-product btn btn-medium js-filter-markers bt-filter filter-showrooms" data-place-type="0">
+            Red Markers
+        </p>
+    </div>
 
-                <!-- MAP + SIDEBAR -->
-                <div id="store-locator-container">
-                    <div id="sidebar">
 
-                        <!-- GEOCODER SEARCH -->
-                        <div class="locator-block-title find-store">
-                            <h1 class="txt-title-l-2 txt-title-dashed">
-                                Search
-                            </h1>
-                            <div class="js-geocoder-search form-item" data-input-placeholder="Saisissez une ville, un CP..."></div>
+    <!-- coords from url -->
+    <?php $lat = isset($_GET['lat']) ? $_GET['lat'] : ''; ?>
+    <?php $lng = isset($_GET['lng']) ? $_GET['lng'] : ''; ?>
 
-                            <button type="button" class="btn btn-medium js-locate-me">
-                                locate me
-                            </button>
+    <!-- MAP + SIDEBAR -->
+    <div id="store-locator-container">
+        <div id="sidebar">
 
-                            <button type="button" class="btn btn-medium decli-reverse btn-back js-display-panel" data-display-panel="1">
-                                clear
-                            </button>
-                        </div>
+            <!-- GEOCODER SEARCH -->
+            <div class="locator-block-title find-store">
+                <h1 class="txt-title-l-2 txt-title-dashed">
+                    Search
+                </h1>
+                <div class="js-geocoder-search form-item" data-input-placeholder="Saisissez une ville, un CP..."></div>
 
-                        <!-- STORES LIST -->
-                        <div class="gutter-left list-title">
-                            <h1 class="txt-title-l-2 txt-title-dashed">
-                                Stores close to
+                <button type="button" class="btn btn-medium js-locate-me">
+                    locate me
+                </button>
+
+                <button type="button" class="btn btn-medium decli-reverse btn-back js-display-panel" data-display-panel="1">
+                    clear
+                </button>
+            </div>
+
+            <!-- STORES LIST -->
+            <div class="gutter-left list-title">
+                <h1 class="txt-title-l-2 txt-title-dashed">
+                    Stores close to
                                 <span class="city-nearby">
                                     &lt;CITY&gt;
                                 </span>
-                                <span class="list-count color-orange">
+                                <span class="list-count">
                                     (x)
                                 </span>
-                            </h1>
-                        </div>
+                </h1>
+            </div>
 
-                        <!-- template : list item -->
-                        <script type="text/template" id="tpl-list-item">
-                            <div class="infos">
+            <!-- template : list item -->
+            <script type="text/template" id="tpl-list-item">
+                <div class="infos">
                                 <span class="txt title">
                                     {{title}}
                                     {{distance}}
                                 </span>
-                                {{address1}}
-                                {{address2}}
+                    {{address1}}
+                    {{address2}}
                                 <span class="txt city">
                                     {{zip}} {{city}}
                                 </span>
-                            </div>
-                        </script>
+                </div>
+            </script>
 
-                        <div id="stores-list">
-                            <!-- tpl destination -->
-                        </div>
-                    </div>
+            <div id="stores-list">
+                <!-- tpl destination -->
+            </div>
+        </div>
 
-                    <!-- THE MAP -->
-                    <div id="map"
-                         data-json-url="json/stores.json"
-                         data-coord-lat="<?php echo $lat; ?>"
-                         data-coord-lng="<?php echo $lng; ?>">
-                    </div>
+        <!-- THE MAP -->
+        <div id="map"
+             data-json-url="json/stores.json"
+             data-coord-lat="<?php echo $lat; ?>"
+             data-coord-lng="<?php echo $lng; ?>">
+        </div>
 
-                    <!-- TEMPLATE : POPUP MARKER -->
-                    <script type="text/template" id="marker-popup-content">
-                        <div class="js-toggle-list-map" data-type="list">
-                            <p class="txt title">
-                                {{title}}
-                            </p>
-                            {{address1}}
-                            {{address2}}
+        <!-- TEMPLATE : POPUP MARKER -->
+        <script type="text/template" id="marker-popup-content">
+            <div class="js-toggle-list-map" data-type="list">
+                <p class="txt title">
+                    {{title}}
+                </p>
+                {{address1}}
+                {{address2}}
                             <span class="txt city">
                                 {{zip}} {{city}}
                             </span>
-                        </div>
-                    </script>
-                </div>
             </div>
-        </div>
+        </script>
     </div>
+
+
 </div>
 
 
